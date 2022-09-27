2aa9dcb332deb8b532ee4804b7368250be7dfcf6
Naoki Takehisa <zohysama@icloud.com>
2022-09-27 10:27:09 +0900

    Initial Commit

---
+++ StudySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudySwiftUI
+//
+//  Created by 武久　直幹 on 2022/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 武久　直幹 on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
