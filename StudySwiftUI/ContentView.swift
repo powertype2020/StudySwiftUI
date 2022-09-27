877f5a200c4fbcc4e17436f171a7e94acbf6d342
Naoki Takehisa <zohysama@icloud.com>
2022-09-27 19:56:43 +0900

    開発初日、四つのViewを用意し、それぞれ簡素に文章・画像・ボタンを配置しました！ まだ別端末でのレイアウトはまだ未実施です。

--- StudySwiftUI/ContentView.swift
+++ StudySwiftUI/ContentView.swift
@@ -7,15 +7,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FirstView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+            VStack {
+                Spacer()
+            Text("九月とはいえまだまだ夏です")
+                .font(.system(size: 22))
+                .multilineTextAlignment(.center)
+                .frame(width: 320.0)
+            Text("体調管理には気を使いましょう！")
+            Image("first")
+                .resizable()
+                .frame(width: 300.0, height: 300.0)
+            Spacer()
+                NavigationLink("次へ", destination: SecondView())
+                    .frame(width: 300.0, height: 80.0)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .clipShape(Capsule())
+                Spacer()
+            }
+        }
+    }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+            VStack {
+                Spacer()
+            Text("日頃の記録を忘れていると…")
+                .font(.system(size: 22))
+                .multilineTextAlignment(.center)
+                .frame(width: 320.0)
+            Text("いつから体調が悪いんだっけ…？")
+            Image("second")
+                .resizable()
+                .frame(width: 300.0, height: 300.0)
+            Spacer()
+            NavigationLink("次へ", destination: ThirdView())
+                    .frame(width: 300.0, height: 80.0)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .clipShape(Capsule())
+            Spacer()
+            }
+            .padding(.bottom)
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+            VStack {
+                Spacer()
+            Text("そんなときにこのアプリ！")
+                .font(.system(size: 22))
+                .multilineTextAlignment(.center)
+                .frame(width: 320.0)
+            Text("さあ！あなたの健康を守りましょう！")
+            Image("mamakarute")
+                .frame(width: 300.0, height: 260.0)
+                .background(Color.pink)
+            Spacer()
+            NavigationLink("スタート！", destination: StartView())
+                    .frame(width: 300.0, height: 80.0)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .clipShape(Capsule())
+            Spacer()
+            }
+            .padding(.bottom)
+    }
+}
+
+struct StartView: View {
+    var body: some View {
+        ZStack {
+            Color.pink
+                .ignoresSafeArea()
+            Spacer()
+            Image("mamakarute")
+                .resizable()
+                .frame(width: 300.0, height: 260.0)
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FirstView()
+        SecondView()
+        ThirdView()
+        StartView()
     }
 }
