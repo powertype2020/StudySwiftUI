f880849b5bf139630913ee9c4b43e82b3a7b71c8
powertype2020 <105772633+powertype2020@users.noreply.github.com>
2022-10-13 19:44:30 +0900

    Revert "現状のコードを更新、サンプルに似たような挙動にはなりましたが今は画像のみがスクロールされる状態。"

--- StudySwiftUI/ContentView.swift
+++ StudySwiftUI/ContentView.swift
@@ -1,41 +1,92 @@
 //
-//  ContentVIew.swift
+//  ContentView.swift
 //  StudySwiftUI
 //
-//  Created by 武久　直幹 on 2022/09/30.
+//  Created by 武久　直幹 on 2022/09/27.
 //
 
-import Foundation
 import SwiftUI
 
-struct WalkthroughView: View {
+struct FirstView: View {
     
-    @ObservedObject var viewModel = WalkthroughViewModel()
+    @State var currentPage = 0
+    @State private var offset: CGFloat = 0
+    @State private var index: Int = 0
     
     var body: some View {
         VStack {
-            WalkthroughDetailView(viewModel: viewModel)
             Spacer()
-            NextButtonView(viewModel: viewModel)
-                .padding(.bottom)
+            Text("題名")
+                .font(.largeTitle)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView(.horizontal, showsIndicators: false) {
+                Image("first").resizable()
+                            }
+            .content.offset(x: self.offset)
+                        .frame(width: geometry.size.width, height: nil, alignment: .leading)
+                        .gesture(DragGesture()
+                            .onChanged({ value in
+                                self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
+                            })
+                            .onEnded({ value in // 4. Dragが完了したら、Drag量に応じて、indexを更新
+                                let scrollThreshold = geometry.size.width / 2
+                                if value.predictedEndTranslation.width < -scrollThreshold {
+                                    if currentPage <= 3 {
+                                    currentPage += 1
+                                    }
+                                } else if value.predictedEndTranslation.width > scrollThreshold {
+                                    if currentPage >= 1 {
+                                    currentPage -= 1
+                                    }
+                                }
+                            })
+                        )
         }
-        .background(Color.pink)
+        .frame(width: 300.0, height: 300.0)
+            Text("ここにViewModelからの変数")
+                .font(.title)
+                .padding(.horizontal)
+            Spacer()
+            pageControl(current: currentPage)
+            Button(action: {
+                if currentPage < 4 {
+                currentPage += 1
+                    print(currentPage)
+                }
+            }) {
+                Text("次へ")
+                    .font(.title)
+            }
+            .frame(width: 300.0, height: 70.0)
+            .foregroundColor(Color.white)
+            .background(Color.pink)
+            .cornerRadius(50)
+            Spacer()
+        }
     }
 }
-
-struct HomeView: View {
-    
-    var body: some View {
-        SerchMusicView()
-            .background(Color.red)
-    }
-}
-
-
-struct WalkthroughView_Previews: PreviewProvider {
-    static let viewModel = WalkthroughViewModel()
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        WalkthroughView()
-        HomeView()
+        FirstView()
     }
+}
+
+struct pageControl: UIViewRepresentable {
+    
+    var current = 0
+    
+    func makeUIView(context: UIViewRepresentableContext<pageControl>) -> UIPageControl {
+        let page = UIPageControl()
+        page.numberOfPages = 5
+        page.pageIndicatorTintColor = .gray
+        return page
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: UIViewRepresentableContext<pageControl>) {
+        uiView.currentPage = current
+    }
+    
+    
+    
 }
--- StudySwiftUI/Model.swift
+++ StudySwiftUI/Model.swift
@@ -7,21 +7,28 @@
 
 import Foundation
 
-struct WTModel: Identifiable, Hashable {
+class Model1: ObservableObject {
     
-    let id: Int
-    let title: String
-    let text: String
-    let imageString: String
+    @Published var title = "はじめまして！"
+    @Published var text = "このアプリは、あなたやお子さまの健康を考えるアプリです！"
     
 }
 
-struct MusicData: Codable {
+class Model2: ObservableObject {
     
-    let trackId: Int
-    let artistName: String
-    let collectionName: String
-    let trackName: String
-    let artworkUrl100: URL
+    @Published var title = "健康管理"
+    @Published var text = "病気になりがちな現代、体温や体調などを記録することは大事です。"
+}
+
+class Model3: ObservableObject {
     
+    @Published var title = "最初はヘルプ！"
+    @Published var text = "まずはじめに、カレンダー画面の左下にあるHelpを確認しよう！"
+    
+}
+
+class Model4: ObservableObject {
+    
+    @Published var title = "Start!"
+    @Published var text = "使い方は自由です！さあ、始めましょう！"
 }
